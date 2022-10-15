180b9768ceb67f4b5efbe863368f6158f3ee83a2
shagun-khedia-40 <95223602+shaggy40@users.noreply.github.com>
2022-10-15 15:07:41 +0530

    Time: 171 ms (35.01%), Space: 35.4 MB (80.73%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -4,18 +4,19 @@ public:
         int emp = logs[0][0];
         int tm = logs[0][1];
         
-        for(int i=1;i<logs.size();i++){
-        int diff = logs[i][1]-logs[i-1][1];
-        
-        if(diff > tm){
-            tm = diff;
-            emp = logs[i][0];
-        }
-        else if(diff==tm){
-            emp = min(emp,logs[i][0]);
-        }
-        
-        
+        for(int i=1;i<logs.size();i++)
+        {
+            int diff = logs[i][1] - logs[i-1][1];
+            
+            if(tm < diff)
+            {
+                tm = diff;
+                emp = logs[i][0];
+            }
+            else if(tm == diff)
+            {
+                emp = min(emp,logs[i][0]);
+            }
         }
         return emp;
     }
